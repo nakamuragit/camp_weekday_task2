026deace18d7ad4c042475e5f79077516059cc95
Nakamura <gireru@hotmail.co.jp>
2019-03-06 04:52:42 +0900

    change animal.rb kadai 2-5

--- animal.rb
+++ animal.rb
@@ -10,3 +10,18 @@ class Animal
     "#{self.name}: #{voice}"
   end
 end
+
+#課題5追加
+class Dog < Animal
+  def initialize(name)
+    super
+    @voice = "ワンワン！"
+  end
+end 
+
+class Cat < Animal
+  def initialize(name)
+    super
+    @voice = "ニャー！"
+  end
+end
